16f2313047f69bf7da8a6cca641a7b75652ab018
joeldart <jdart@remember.com>
2013-03-29 18:03:23 -0400

    added text to littlecoder

---
+++ littlecoder/littlecoder.js
@@ -0,0 +1,30 @@
+(function (forRubyJean) {
+  var littlCoder,yourWay, advice, noop=function(){},willPeople={say:noop},p,e,rformance=[1,1],teamwork=0,moreClear=1,or={toMake:noop},dontLet=function(){return{"is":noop};},advice,truly,good,though=noop,forAll=noop,love=function(some){return some;},comes=noop,out,of=out=love,deserves=noop,respect,asIt={should:noop},mustFight="fighter",avoid=noop,yawn,statements={toBuild:noop},theTruth,thatLives=noop,beforeThe=andAll,here={fighter:noop};
+
+  littleCoder; on(yourWay,                           function 
+    (much) {
+      advice = willPeople.say();
+      for (p,e=rformance                             .length;p<e;p++){
+        teamwork++;
+        or.toMake(++moreClear)();
+      }
+      dontLet(yourImagination).gray();
+   });
+
+   andAll(this, advice)["is"](                        function
+       (truly, good) {
+     though("its often misunderstood");
+     forAll(that, comes(out(of(love))));
+     deserves(respect, asIt.should);
+   });
+
+                                                       var
+   but, you=mustFight;avoid("a",yawn);statements.toBuild();
+
+   function on(mustState){ theTruth =
+     thatLives("in", you);
+     beforeThe(you in here)["is"]("gone");
+   }
+
+
+}("goodnight"));
