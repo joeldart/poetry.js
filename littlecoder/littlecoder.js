d6534a1266103ac237ef7c1c6d7bddbc6891fd51
Joel Dart <jdart@remember.com>
2013-08-03 23:45:15 -0400

    formated the variable block to look more like a forest
    had this great variable block from which all this great hoisting and declaration comes from but it was just sitting there on the page being a single line. Reformatted the whitespace to make a forest path that the narration runs right through (note the width is more narrow than the paragraph so you get a nice feeling of being cramped as you move down the path).

--- littlecoder/littlecoder.js
+++ littlecoder/littlecoder.js
@@ -1,33 +1,40 @@
 (function (forRubyJean) {
 
-  littleCoder; on(yourWay,                           function 
-    (much) {
-      advice = willPeople.say();
-      for (p,e=rformance                             .length;p<e;p++){
-        teamwork++;
-        or.toMake(++moreClear)();
-      }
-      dontLet(yourImagination).gray();
-   });
+                                           littleCoder; on(yourWay,                           function 
+                                             (much) {
+                                               advice = willPeople.say();
+                                               for (p,e=rformance                             .length;p<e;p++){
+                                                 teamwork++;
+                                                 or.toMake(++moreClear)();
+                                               }
+                                               dontLet(yourImagination).gray();
+                                            });
+           
 
+  var   littleCoder,       yourWay, advice,              dontLet=function(){return{"is":noop};},              out,
+  willPeople =  { say:noop}, p,though=noop,              advice,truly,good, e, love=function(some){return  some;},
+  rformance=[1,1], teamwork= 0,moreClear=1,              or={toMake:noop},forAll=noop, comes=noop, of=out=   love,
+  deserves=noop,respect,asIt={should:noop},              mustFight="fighter",statements={toBuild:noop},  theTruth,
+  avoid       =     noop,             yawn,              thatLives=noop,andAll = beforeThe,here;function noop(){};
+  function thatLives(){      you="fighter";              here={fighter:noop};}; function beforeThe(){return {"is":
+  noop};};
+  
+  
+  
+                                           andAll(this, advice)["is"](                        function
+                                             (truly, good) {
+                                              though("its often misunderstood");
+                                              forAll(that, comes(out(of(love))));
+                                              deserves(respect, asIt.should);
+                                           });
 
-  var littleCoder,yourWay, advice, willPeople={say:noop},p,e,rformance=[1,1],teamwork=0,moreClear=1,or={toMake:noop},dontLet=function(){return{"is":noop};},advice,truly,good,though=noop,forAll=noop,love=function(some){return some;},comes=noop,out,of=out=love,deserves=noop,respect,asIt={should:noop},mustFight="fighter",avoid=noop,yawn,statements={toBuild:noop},theTruth,thatLives=noop,andAll = beforeThe,here;function noop(){};function thatLives(){you="fighter";here={fighter:noop};};function beforeThe(){return {"is":noop};};
+                                                                  var
+                                            but, you=mustFight;avoid("a",yawn);statements.toBuild();
 
-
-   andAll(this, advice)["is"](                        function
-       (truly, good) {
-     though("its often misunderstood");
-     forAll(that, comes(out(of(love))));
-     deserves(respect, asIt.should);
-   });
-
-                                                       var
-   but, you=mustFight;avoid("a",yawn);statements.toBuild();
-
-   function on(mustState){ theTruth =
-     thatLives("in", you);
-     beforeThe(you in here)["is"]("gone");
-   }
+                                            function on(mustState){ theTruth =
+                                              thatLives("in", you);
+                                              beforeThe(you in here)["is"]("gone");
+                                            }
 
 
 }("goodnight"));
