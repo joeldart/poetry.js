0a90352dbc6f1f184b662a6a002de7cc6b942e7b
Joel Dart <jdart@DyKnows-MacBook-Pro.local>
2015-01-12 23:23:09 -0500

    added for byron and ada

---
+++ for_byron_and_ada/README.md
@@ -0,0 +1,14 @@
+#### Artist
+Kathryn Barnes Dart, Joel Dart, Emily Hinkel, Whitney Guthrie
+Indianapolis, IN USA and Henderson, KY USA
+
+#### Artist Statement
+This piece is inspired by famed poet Lord Byron and his daughter
+Ada Lovelace, the first computer programmer. It considers the 
+connection between creative pursuits and the backing spirit of
+creativity that makes all of it possible.
+
+The poem mostly consists of object graphs, resembling trees. But two trees are 
+called out explicitly in the poem via their assignment of their 
+prototypes. Later on, fitting the text, these trees intertwine
+but do so with the very object that is their shared root.
---
+++ for_byron_and_ada/for_byron_and_ada.js
@@ -0,0 +1,20 @@
+                                                        var
+take, root = {}, and; hear = { of : { trees: { } } };   var
+up = { they: { grow: {} }, over: { and: { over: {} } } },
+theBranches = { reach: { 
+    ever: { higher: { 
+            towards: { the : {sun: { } } } 
+        } } 
+    }
+};
+up.                                         __proto__ = 
+theBranches.                                __proto__ = 
+({reach: {}}, and = root);                        var 
+inThe = { rich: { soilOf: { the: { sky: {} } } } },
+
+but = { pause : { toConsider: {} } },
+                                    
+two = { trees: { grow: {}, upward: {} } },
+next = { to: { each: {}, other: {} } };
+up.over.years = 
+and; theBranches.reach.out = and; ({intertwine:{}});
